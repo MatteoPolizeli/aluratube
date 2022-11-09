b26bace4712720cec4142850859429cfd6fdbb25
maddeo.exe <polizeli.contato@gmail.com>
2022-11-08 21:33:32 -0300

    first commit

---
+++ src/components/banner.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import config from "../../config.json";
+
+export const StyledBanner = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 230px;
+  padding-top: 56px;
+
+  img{
+    width: 100%;
+    height: 230px;
+  }
+`;
